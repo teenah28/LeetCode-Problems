04584447d1e4bdda58a61f91dd033baad7f09d6d
Teena Hotchandani <137616924+teenah28@users.noreply.github.com>
2024-06-11 20:03:21 +0530

    Time: 2 ms (83.93%), Space: 45.9 MB (66.59%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,19 +1,41 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        List<Integer> temp=new ArrayList<>();
+        //Approach 1
+        // List<Integer> temp=new ArrayList<>();
 
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         temp.add(nums[i]);
+
+        //     }
+        // }
+        // for(int i=0;i<temp.size();i++){
+        //     nums[i]=temp.get(i);
+        // }
+        // int nonzero=temp.size();
+        // for(int i=nonzero;i<nums.length;i++){
+        //     nums[i]=0;
+        // }
+
+        //Approach 2
+        int j=-1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                temp.add(nums[i]);
-
+            if(nums[i]==0){
+                j=i;
+                break;
             }
         }
-        for(int i=0;i<temp.size();i++){
-            nums[i]=temp.get(i);
+        if(j==-1){
+            return;
         }
-        int nonzero=temp.size();
-        for(int i=nonzero;i<nums.length;i++){
-            nums[i]=0;
+        for(int i=j+1;i<nums.length;i++){
+            if(nums[i]!=0){
+                int tmp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=tmp;
+                j++;
+            }
+            
         }
     }
 }
