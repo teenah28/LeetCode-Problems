e1f02592493df0d00c88b663421cf2a93d8bae34
Teena Hotchandani <137616924+teenah28@users.noreply.github.com>
2024-07-22 14:58:21 +0530

    Time: 0 ms (100%), Space: 41.6 MB (20.85%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -14,17 +14,18 @@
  * }
  */
 class Solution {
-    //preorder : root ,left , right
-     List<Integer> list=new ArrayList<>();
-     void preorder(TreeNode root){
-        if(root==null) 
-            return ;
+    List<Integer> list =new ArrayList<>();
+    void preorder(TreeNode root){
+        if(root==null){
+            return;
+        }
         list.add(root.val);
         preorder(root.left);
         preorder(root.right);
     }
     public List<Integer> preorderTraversal(TreeNode root) {
+        //preorder traversal : root left right
         preorder(root);
-        return list;
+    return list;
     }
 }
